304ea0663baa414d330d50f57be77ce81b90057d
TheGreatProgrammerMe <56173158+TheGreatProgrammerMe@users.noreply.github.com>
2019-10-16 21:43:30 +0300

    Update views.py

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -6,40 +6,44 @@ from django.core.paginator import Paginator
 from urllib.parse import urlparse
 
 def index(request):
-    return redirect(reverse(bus_stations))
-
+	return redirect(reverse(bus_stations))
 
 def bus_stations(request):
-    objectreader = []
-    current_page = 1
-    page_num = request.GET.get('page')
-    if (page_num is not None):
-    	current_page = int(page_num)
-    countp = 10
-    with open(settings.BUS_STATION_CSV, newline='') as csvfile:
-	    reader = csv.DictReader(csvfile)
-	    for row in reader:
-	    	objectreader.append([row['Name'],row['Street'],row['District']])
+	current_page = 1
+	objectreader = []
+	page_num = request.GET.get('page')
 
-    paginator = Paginator(objectreader, countp)
-    page = paginator.get_page(current_page)
-    datalist = page.object_list
-    dN = []
-    dS = []
-    dD = []
-    for i in datalist:
-    	dN.append(i[0])
-    	dS.append(i[1])
-    	dD.append(i[2])
-    dN = '<br>'.join(dN)
-    dS = '<br>'.join(dS)
-    dD = '<br>'.join(dD)
+	if (page_num is not None):
+		current_page = int(page_num)
 
-    next_page_url = reverse('bus_stations') + '?page=' + str(current_page+1)
+	with open(settings.BUS_STATION_CSV, newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			tlist = (
+				{'Name' : row['Name'], 
+				'Street' : row['Street'], 
+				'District' : row['District']})
+			objectreader.append(tlist)
 
-    return render_to_response('index.html', context={
-        'bus_stations': [{'Name': dN, 'Street': dS, 'District': dD}],
-        'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
-    })
+	paginator = Paginator(objectreader, 10)
+	page = paginator.get_page(current_page)
+	datalist = page.object_list
+	
+	if page.has_next():
+		next_page_url = reverse('bus_stations') + '?page=' + str(page.next_page_number())
+		if (page_num is not None): current_page = page.next_page_number()-1
+	else:
+		next_page_url = None
+		
+	if page.has_previous():
+		previous_page_url = reverse('bus_stations') + '?page=' + str(page.previous_page_number())
+		if (page_num is not None): current_page = page.previous_page_number()+1
+	else:
+		previous_page_url = None
+
+	return render_to_response('index.html', context={
+		'bus_stations': datalist,
+		'current_page': current_page,
+		'prev_page_url': previous_page_url,
+		'next_page_url': next_page_url,
+	})
